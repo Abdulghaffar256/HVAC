12811d81982ea06c73aaea13dd43b71fc163976b
Abdulghaffar256 <113060994+Abdulghaffar256@users.noreply.github.com>
2025-04-30 05:17:34 -0700

    Update page.js

--- src/app/blogs/page.js
+++ src/app/blogs/page.js
@@ -8,29 +8,29 @@ export default function Developmentf() {
       {/* Adding the Head component for Open Graph and Twitter meta tags */}
       <Head>
         <title> HVAC Blogs </title>
-        <meta property="og:title" content="  web Development using Next Js" />
+        <meta property="og:title" content="  Educational websites help students read informative hvac blogs  and enroll in the best online free hvac courses and free hvac reated tools" />
         <meta
           property="og:description"
-          content="Educational websites help students read informative blogs  and enroll in the best online courses related to  web Development using Next Js"
+          content="Educational websites help students read informative hvac blogs  and enroll in the best online free hvac courses and free hvac reated tools"
         />
-        <meta property="og:image" content="https://www.epicssolution.com/social-banner.png" />
-        <meta property="og:url" content="https://www.epicssolution.com/" />
+        <meta property="og:image" content="https://www.hvacdesigning.com/social-banner.png" />
+        <meta property="og:url" content="https://www.hvacdesigning.com/" />
         <meta property="og:type" content="website" />
         
         {/* Twitter Card Meta Tags */}
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="  web Development using Next Js courses & Blogs" />
+        <meta name="twitter:title" content="  Educational websites help students read informative hvac blogs  and enroll in the best online free hvac courses and free hvac reated tools" />
         <meta
           name="twitter:description"
-          content="Educational websites help students read informative blogs  and enroll in the best online courses related to use of  web Development using Next Js."
+          content="Educational websites help students read informative hvac blogs  and enroll in the best online free hvac courses and free hvac reated tools"
         />
-        <meta name="twitter:image" content="https://www.epicssolution.com/social-banner.png" />
+        <meta name="twitter:image" content="https://www.hvacdesigning.com/social-banner.png" />
       </Head>
       
       <div className="mt-8">
         {/* Second component */}
         <div className="flex justify-center align-middle font-semibold text-2xl border-[1px] border-solid border-dark dark:border-light text-black dark:text-light rounded-lg p-4 sticky top-6 max-h-[80vh]" >
-                 <h1>       Next js (Web Development) Blogs & Courses  </h1>
+                 <h1>       HVAC Blogs  </h1>
 
         </div>
        <div className=" mt-6 "> <article  style={{ minHeight: '300px', width: '100%' }}>
