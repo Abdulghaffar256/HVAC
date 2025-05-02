3f9650fe2bc43c687b742c63f5c2fd0c87a67905
Abdulghaffar256 <113060994+Abdulghaffar256@users.noreply.github.com>
2025-05-02 15:27:11 +0500

    Update siteMetaData.js

--- src/utils/siteMetaData.js
+++ src/utils/siteMetaData.js
@@ -1,11 +1,11 @@
 const siteMetadata = {
-  title: 'Epics Solution ',
-  author: 'Epics',
+  title: 'HVAC Designing ',
+  author: 'HVAC',
   headerTitle: 'Blog',
-  description: 'Educational websites help students get Knowledge  Use of AI in HVAC, Energy & Development.',
+  description: 'Master HVAC design with theory, calculations, and hands-on projects. Learn load/ductwork sizing, industry tools, and energy-efficient solutions for residential/commercial systems. Ideal for engineers, technicians, and students. Enroll now! ',
   language: 'en-us',
   theme: 'dark', // system, dark or light
-  siteUrl: 'https://www.epicssolution.com/', // your website URL
+  siteUrl: 'https://www.hvacdesigning.com/', // your website URL
   siteLogo: '/logo.png',
   email: 'abdulghaffar25600@epicssolution.com',
   facebook: 'https://www.facebook.com/EPICS256',
