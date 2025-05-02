84c91e4a3e73469d51b8779f51dd0990e43f7c72
Abdulghaffar256 <113060994+Abdulghaffar256@users.noreply.github.com>
2025-05-02 21:24:57 +0500

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,6 @@
 import "./globals.css";
 import { cx } from "@/utils";
 import { Inter, Manrope } from "next/font/google";
-import Script from "next/script";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import siteMetadata from "@/utils/siteMetaData";
@@ -71,49 +70,11 @@ export default function RootLayout({ children }) {
           type="font/woff2"
           crossOrigin="anonymous"
         />
-        <meta name="author" content="Epic Solutions" />
-        <meta
-          name="google-site-verification"
-          content="69LCpKE_Jr39yMWMRpX-mJW1E7RjEi9rrvimR4Qh2KY"
-        />
-        <meta name="p:domain_verify" content="be8a436f4e7ce83fbeafd23c184e415b" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-
-        <Script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4108246218082712"
-          crossOrigin="anonymous"
-          strategy="afterInteractive"
-        />
       </head>
 
       <body
         className={cx(inter.variable, manrope.variable, "font-mr bg-light dark:bg-dark")}
       >
-        <Script id="theme-switcher" strategy="beforeInteractive">
-          {`
-            if (localStorage.getItem('theme') === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-              document.documentElement.classList.add('dark');
-            } else {
-              document.documentElement.classList.remove('dark');
-            }
-          `}
-        </Script>
-
-        <Script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-CHNLXSTLH7"
-          strategy="afterInteractive"
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-CHNLXSTLH7');
-          `}
-        </Script>
-
         <Header />
         <main>{children}</main>
         <Footer />
