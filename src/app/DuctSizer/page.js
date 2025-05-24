21c94e35d1d1f9a7b8fcb35cc97abe67d87cf309
Abdulghaffar256 <113060994+Abdulghaffar256@users.noreply.github.com>
2025-05-24 12:26:02 +0500

    Create page.js

---
+++ src/app/DuctSizer/page.js
@@ -0,0 +1,9 @@
+import DuctSizerCalculator from '@/components/DuctSizerCalculator';
+
+export default function DuctSizerPage() {
+  return (
+    <div className="min-h-screen bg-gray-100 dark:bg-gray-900 p-4">
+      <DuctSizerCalculator />
+    </div>
+  );
+}
