822fbba268a91eb621c1493e1cde5113a7399bf1
Abdulghaffar256 <113060994+Abdulghaffar256@users.noreply.github.com>
2025-07-22 17:45:47 +0500

    Update page.js

--- src/app/Tools/page.js
+++ src/app/Tools/page.js
@@ -6,7 +6,7 @@ import Link from "next/link";
 const tools = [
   {
     name: "Load Calculator",
-    image: "/images/loadcalc.jpg",
+    image: "/loadcalc.jpg",
     link: "/loadcalculator",
     description: "Calculate heating and cooling loads with ease and accuracy.",
   },
