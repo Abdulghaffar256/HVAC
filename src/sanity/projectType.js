8668a3abaf9b8ae5691ee219d215c589338f4c67
Abdulghaffar256 <113060994+Abdulghaffar256@users.noreply.github.com>
2025-08-01 14:27:54 +0500

    Update projectType.js

--- src/sanity/projectType.js
+++ src/sanity/projectType.js
@@ -2,7 +2,7 @@ import { defineField, defineType } from 'sanity';
 
 export const projectType = defineType({
   name: 'project',
-  title: 'Project ',
+  title: 'Project Management Post ',
   type: 'document',
   fields: [
     defineField({
