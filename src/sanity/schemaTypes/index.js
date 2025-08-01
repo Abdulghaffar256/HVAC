cbe3ac24641816e12e0f81ae1037af7ba80c99e9
Abdulghaffar256 <113060994+Abdulghaffar256@users.noreply.github.com>
2025-08-01 17:16:50 +0500

    Update index.js

--- src/sanity/schemaTypes/index.js
+++ src/sanity/schemaTypes/index.js
@@ -4,9 +4,8 @@ import { engType } from "../engType";
 import { devType } from "../devType";
 import { equipmentType } from "../equipmentType";
 import { projectType } from "../projectType";
-import { ProjectType } from "../ProjectType1";
 
 
 export const schema = {
-  types: [blogType, equipmentType, coursesType, engType, devType, projectType, ProjectType],
+  types: [blogType, equipmentType, coursesType, engType, devType, projectType],
 };
