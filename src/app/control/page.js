4b6f4ff3b7ef93dfd5ab365ce755d6b74d044875
Sameen-fatima <sf247110@gmail.com>
2025-08-23 23:46:46 +0500

    Update page.js

--- src/app/control/page.js
+++ src/app/control/page.js
@@ -1,43 +1,60 @@
 import React from "react";
 import Engineering from "@/components/engineering/page";
-import Head from 'next/head'; // Importing Head component for adding meta tags
+import Head from "next/head";
 
 export default function Eng() {
   return (
     <>
-      {/* Adding the Head component for Open Graph and Twitter meta tags */}
+      {/* Meta Tags for SEO and Social Sharing */}
       <Head>
-        <title>HVAC Control </title>
-        <meta property="og:title" content="Best online courses related to hvac control in this course you learn about hvac controls this is free  " />
+        <title>HVAC Control</title>
+        <meta
+          property="og:title"
+          content="Best online courses related to HVAC control. In this course, you will learn about HVAC controls for free."
+        />
         <meta
           property="og:description"
-          content="Best online courses related to hvac control in this course you learn about hvac controls this is free  "
+          content="Best online courses related to HVAC control. In this course, you will learn about HVAC controls for free."
         />
-        <meta property="og:image" content="https://www./www.hvacdesigning.com/social-banner.png" />
-        <meta property="og:url" content="https://www.hvacdesigning.com/" />
+        <meta
+          property="og:image"
+          content="https://www.hvacdesigning.com/social-banner.png"
+        />
+        <meta
+          property="og:url"
+          content="https://www.hvacdesigning.com/"
+        />
         <meta property="og:type" content="website" />
-        
+
         {/* Twitter Card Meta Tags */}
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="Best online courses related to hvac control in this course you learn about hvac controls this is free " />
+        <meta
+          name="twitter:title"
+          content="Best online courses related to HVAC control. In this course, you will learn about HVAC controls for free."
+        />
         <meta
           name="twitter:description"
-          content="Best online courses related to hvac control in this course you learn about hvac controls this is free "
+          content="Best online courses related to HVAC control. In this course, you will learn about HVAC controls for free."
         />
-        <meta name="twitter:image" content="https://www./www.hvacdesigning.com/social-banner.png" />
+        <meta
+          name="twitter:image"
+          content="https://www.hvacdesigning.com/social-banner.png"
+        />
       </Head>
-      
-      <div className="mt-8">
-        {/* Second component */}
-        <div className="flex justify-center align-middle font-semibold text-2xl border-[1px] border-solid border-dark dark:border-light text-black dark:text-light rounded-lg p-4 sticky top-6 max-h-[80vh]" >
-             <h1>       HVAC controls   </h1>
- 
 
+      <div className="mt-8">
+        {/* Heading Section */}
+        <div className="flex justify-center items-center font-semibold text-2xl border border-dark dark:border-light text-black dark:text-light rounded-lg p-4 sticky top-6 max-h-[80vh]">
+          <h1>HVAC Controls</h1>
         </div>
-       <div className=" mt-6 "> <article  style={{ minHeight: '300px', width: '100%' }}>
-          <Engineering />
-        </article></div>
+
+        {/* Engineering Component */}
+        <div className="mt-6">
+          <article style={{ minHeight: "300px", width: "100%" }}>
+            <Engineering />
+          </article>
+        </div>
       </div>
-    </>
-  );
+    </>
+  );
 }
